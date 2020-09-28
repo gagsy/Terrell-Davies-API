668a84a91543cba9fdccd9047892f21b74b9e8c5
Tope Olotu <topeolotu75@gmail.com>
2020-09-28 11:38:37 +0100

    Admin login apis

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', 'Api\AuthController@login')->name('login');
 
+Route::post('/admin-login', 'Api\AuthController@AdminLogin');
+
 Route::post('register', 'Api\AuthController@register')->name('register');
 
 Route::get('check-auth', 'Api\AuthController@checkAuth')->name('logout');
