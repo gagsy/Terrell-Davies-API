209b12188c895919e01cdde1f30253e49ecab3cd
Seyi Onifade <xyluz@ymail.com>
2020-12-11 16:25:59 +0100

    flutterwave payment service keys

--- app/Services/FlutterwavePaymentService.php
+++ app/Services/FlutterwavePaymentService.php
@@ -4,5 +4,17 @@ use Illuminate\Support\Facades\Http;
 
 class FlutterwavePaymentService 
 {
+    protected $__publicKey;
+    protected $__secretKey;
+    protected $__encryptionKey;
+
+    public function __construct()
+    {
+        //setup api key things here
+        $this->__publicKey = env('APP_ENV') == 'local' ? env('FLUTTERWAVE_PUBLIC_TEST_KEY') : env('FLUTTERWAVE_PUBLIC_KEY');
+        $this->__secretKey = env('APP_ENV') == 'local' ? env('FLUTTERWAVE_SECRET_TEST_KEY') : env('FLUTTERWAVE_SECRET_KEY');
+        $this->__encryptionKey = env('APP_ENV') == 'local' ? env('FLUTTERWAVE_ENCRYPTION_TEST_KEY') : env('FLUTTERWAVE_ENCRYPTION_KEY');
+        
+    }
     
 }
