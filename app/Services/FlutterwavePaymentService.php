0d89cfff83c951bd39835c99f819e6cb30d8519d
Seyi Onifade <xyluz@ymail.com>
2020-12-11 16:33:19 +0100

    create plan service

--- app/Services/FlutterwavePaymentService.php
+++ app/Services/FlutterwavePaymentService.php
@@ -16,5 +16,36 @@ class FlutterwavePaymentService
         $this->__encryptionKey = env('APP_ENV') == 'local' ? env('FLUTTERWAVE_ENCRYPTION_TEST_KEY') : env('FLUTTERWAVE_ENCRYPTION_KEY');
         
     }
+
+    public function createPlan($data){
+        //create subscription plans
+        /** Data sample to send
+         * {
+         *   "amount": 5000,
+         *  "name": "Church collections plan",
+         *   "interval": "monthly",
+         *   "duration": 48
+         *   }
+         */
+
+        $response = Http::withHeaders([
+            'Authorization' => "Bearer " . $this->__publicKey,
+            'Content-Type' => 'application/json'
+        ])
+        ->withOptions(['verify' => false]) //take this out in production
+        ->post("https://api.goshippo.com/shipments/", $data);
+    }
+
+    public function fetchPlanDetails(){
+        //return plan details
+    }
+
+    public function purchasePlan(){
+        //make plan purchase
+    }
+
+    public function registerCustomer(){
+        //register customer in flutterwave
+    }
     
 }
