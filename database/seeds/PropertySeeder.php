dbd62322856e926dc56a1bc32b620d06f2ea4d81
Seyi Onifade <xyluz@ymail.com>
2020-12-16 14:47:19 +0100

    properties seeder

--- database/seeds/PropertySeeder.php
+++ database/seeds/PropertySeeder.php
@@ -11,6 +11,10 @@ class PropertySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $count = (int)$this->command->ask('How many properties do you need ?', 50);
+        $this->command->info("Creating {$count} properties.");
+        $genres = factory(App\Property::class, $count)->create();
+        $this->command->info('Properties Created!');
+
     }
 }
