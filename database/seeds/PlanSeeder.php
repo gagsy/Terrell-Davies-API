7309f35b4e605a90829fbc7a3e8aac71b079788c
Seyi Onifade <xyluz@ymail.com>
2020-12-23 15:03:03 +0100

    basic plan seeder

--- database/seeds/PlanSeeder.php
+++ database/seeds/PlanSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Plan;
 class PlanSeeder extends Seeder
 {
     /**
@@ -11,6 +11,18 @@ class PlanSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Plan::create([
+
+            'name' => 'basic',
+            'price' => 0.0,
+            'duration' => '100',
+            'discount_month1' => '',
+            'discount_month2' =>  '',
+            'maximum_listings' => 5,
+            'maximum_premium_listings' =>0,
+            'max_featured_ad_listings' => 0,
+            'gateway_id'=> '123'
+
+        ]);
     }
 }
